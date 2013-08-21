3f317b9812611fc7f22e5bcd973c42f8dc56fe35
Yunhong Min <yhmin84@gmail.com>
2013-08-21 17:49:31 +0000

    Update grader.js and checks.json

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://nameless-dawn-1418.herokuapp.com"
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,6 +67,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to url', clone(assertFileExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
